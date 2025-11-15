8a24185c917eda5e4b6386314e1054f60908a0be
Santa Tollimus <santatollimus@gmail.com>
2025-11-15 12:58:10 -0600

    Remove vorp_hunting.zip

--- RESOURCES/[coalcounty]/coal_horse_stamina/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'rdr3'
-
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-client_scripts {
-    'client.lua'
-}
